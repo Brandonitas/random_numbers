83f4e9475807f0923173c055d365d6837cc9e263
Brandon Reyes <brandon.reyesmi@gmail.com>
2020-04-18 21:25:16 -0500

    Lineal combinado finished

--- src/app/components/congruencial-lineal-combinado/congruencial-lineal-combinado.component.ts
+++ src/app/components/congruencial-lineal-combinado/congruencial-lineal-combinado.component.ts
@@ -26,8 +26,18 @@ import { MatSnackBar } from "@angular/material";
 export class CongruencialLinealCombinadoComponent implements OnInit {
   public semilla: any = [];
   public random: any = [];
-  public result: any = [];
+  public result = [];
   public randomService = new RandomService();
+  public nRandoms = [];
+  public seed = [];
+  public a = [];
+  public c = [];
+  public m = [];
+  public operador = [];
+  public n = 0;
+  
+
+  
 
   constructor(public snackBarSuccess: MatSnackBar,
               public snackBarError: MatSnackBar) { }
@@ -35,8 +45,64 @@ export class CongruencialLinealCombinadoComponent implements OnInit {
   ngOnInit() {
   }
 
-  linealCombinado = () =>{
+  linealCombinado = (quantity, modulo) =>{
+    this.cleanData();
+
+    modulo = parseInt(modulo);
+    quantity = parseInt(quantity)
     
+    /*let isValid = this.isValidCongruencial(seed,quantity,a,c,m);
+    if(!isValid){
+      return;
+    }*/
+
+    //Si es valido abrir dialogo de exito
+    this.openSuccessDialog();
+
+    let finalResult = [];
+    //this.semilla.push(seed)
+    for(let i = 0; i<=this.n;i++){
+      let counter = 0;
+      let next_seed: number;
+      this.result[i] = [];
+      let previous_seed = this.seed[i];
+      while (counter < quantity) {
+          next_seed = (this.a[i] * previous_seed + this.c[i]) % this.m[i];
+          this.result[i][counter] = next_seed;
+          previous_seed = next_seed;
+          counter += 1
+      }
+      
+    }
+    /*
+    [0,0 = 4][0,1][0,2][*][]
+    [3][][][][]
+    [5][][][][]
+    [10][][][][]
+
+    [4+3+3+5+5+10+10][][][][]
+    */
+
+    for(let i = 0; i<quantity;i++){
+      let counter = 0;
+      finalResult[i] = this.result[0][i];
+      while(counter<this.n){
+        switch(this.operador[counter]){
+          case '+':
+            finalResult[i] += this.result[counter+1][i];
+            break;
+          case '-':
+            finalResult[i] -= this.result[counter+1][i];
+            break;          
+        }
+        counter += 1
+      }
+    }
+
+    console.log("RESULTS",this.result);
+    console.log("FINAL RESULTS", finalResult);
+
+
   }
 
   openErrorDialog(error){
@@ -58,6 +124,44 @@ export class CongruencialLinealCombinadoComponent implements OnInit {
     this.random = [];
     this.result = [];
   }
-  
+
+  addNewGenerator(){
+    this.nRandoms.push(1);
+    console.log("N", this.nRandoms.length)
+    this.n = this.nRandoms.length-1;
+    //this.seed[n] = (<HTMLInputElement>document.getElementById('seed0')).value;
+
+    console.log("ESTE ES MI VALOR WII",this.seed[this.n]);
+
+  }
+
+  addGenerator($scope){
+ 
+  }
+
+  onKeySeed(event: any ,i:number){
+    this.seed[i] = parseInt(event.target.value);
+    console.log("SEED"+i, this.seed[i]);
+  }
+
+  onKeyA(event: any ,i:number){
+    this.a[i] = parseInt(event.target.value);
+    console.log("A"+i, this.a[i]);
+  }
+
+  onKeyC(event: any ,i:number){
+    this.c[i] = parseInt(event.target.value);
+    console.log("C"+i, this.c[i]);
+  }
+
+  onKeyM(event: any ,i:number){
+    this.m[i] = parseInt(event.target.value);
+    console.log("M"+i, this.m[i]);
+  }
+
+  onKeyOperador(event: any ,i:number){
+    this.operador[i] = event.target.value;
+    console.log("OPERADOR"+i, this.operador[i]);
+  }
 
 }
